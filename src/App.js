e835b6788027268c3d6bd802e9d1a1a6f848d294
IOTAIROBERTO <124071310+IOTAIROBERTO@users.noreply.github.com>
2023-02-10 00:49:51 -0700

    Chart.js Integrattion and Implementation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+// App.js
+import { useState } from "react";
+import { data } from "./utils/Data";
+import PieChart from "./components/PieChart";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import RadarChart from "./components/RadarChart";
+import "./App.css";
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -6,11 +14,32 @@ import {
 import Home from "./Home";
 import About from "./About"; 
 import Contact from "./Contact"; 
-import { StyledLink } from "./styles"; 
+import { StyledLink } from "./styles";   
+import Chart from 'chart.js/auto';  
 
-const App = () => {
+export default function App() {
+  const [chartData, setChartData] = useState({
+    labels: data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
+
   return (
-    <section className="App">
+    <>
+      <section className="App">
       <Router>
         <nav>
           <StyledLink to="/">Home</StyledLink>
@@ -24,7 +53,14 @@ const App = () => {
         </Routes> 
       </Router>
     </section>  
-  );
-};
 
-export default App;
+      <div className="App">
+        <PieChart chartData={chartData} /> 
+        <BarChart chartData={chartData} />      
+        <LineChart chartData={chartData} />
+        <RadarChart chartData={chartData} />
+      </div>
+    </>
+    
+  );
+} 
