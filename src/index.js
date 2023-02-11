f9c1c581af15b5c31e43708c28d93d8bc4b070b3
CloudPromavsa <95986505+CloudPromavsa@users.noreply.github.com>
2023-02-10 19:22:28 -0700

    Aws Dynamo integration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom"; 
 import App from "./App";
 
+//AWS STARTS HERE
+import * as AWS from 'aws-sdk'
+import { ConfigurationOptions } from 'aws-sdk'
+
+var configuration = {
+    region: 'us-east-2',
+    secretAccessKey: 'AKIAVTQBHSP3X5PFALPM',
+    accessKeyId: 'K1iIvq4tpCZoKL2X5pYlIK1XzikJF/VtPfQ9FSiL'
+};
+
+AWS.config.update(configuration)
+// AWS ENDS HERE
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
