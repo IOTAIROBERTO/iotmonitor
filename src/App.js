3a949af7dc4691b260f581b3864843ae45195d6f
RobertoMoreno <roberto.moreno@cloudsourceit.com>
2023-10-23 08:38:44 -0700

    .

--- src/App.js
+++ src/App.js
@@ -18,59 +18,105 @@ import React from "react";
 // import { StyledLink } from "./styles";
 import Chart from 'chart.js/auto';
 import "./App.css";
-//import { GetItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
-const AWS = require('aws-sdk');   
+import { TabulatorFull as Tabulator } from 'tabulator-tables';
+const AWS = require('aws-sdk');
 const DynamoDB = new AWS.DynamoDB({ region: "us-east-2", accessKeyId: 'AKIAVTQBHSP373NE63NH', secretAccessKey: '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' });
-const { /*marshall,*/ unmarshall } = require("@aws-sdk/util-dynamodb");
+
 let myChart = null;
 
 export default function App() 
 {
-  var ddbData = {}; 
+  var ddbData = {};
   var data = {};
   var ctx;
-  let configuration = 0;  
+  let configuration = 0;
 
   let params = {
-    TableName: 'iotaimonitor',
+    TableName: 'iotmonitor',
     KeyConditionExpression: 'id = :hashKey',
     ExpressionAttributeValues: {
-      ":hashKey": {"S": "raspberry"}
+      ":hashKey": { "S": "raspberry" }
     },
     Limit: 5,
     ScanIndexForward: false
-  }   
- 
-  setInterval(fetchDataFormDynamoDb, 1500);
+  }
+
+  setInterval(fetchDataFormDynamoDb, 16000);
   //fetchDataFormDynamoDb()
 
-  async function fetchDataFormDynamoDb() 
-  {
+  async function fetchDataFormDynamoDb() {
     // ddbData = await DynamoDB.query(params).promise().then((data,err) => {    
     // console.log(data,err);  
     //   return data;
     // });   
- 
-      try { 
-          ddbData = await DynamoDB.query(params).promise() 
-      } catch (error) {
-          console.error(error);
-      } 
-  
+
+    try {
+      ddbData = await DynamoDB.query(params).promise()
+    } catch (error) {
+      console.error(error);
+    }
 
     //for (let i = 0; i < 4; i += 1)  console.log(ddbData['Items'][i]['record'].N);         
     //for (let i = 0; i < 4; i += 1)  console.log(parseFloat(ddbData['Items'][i]['payload']['M']['distance'].S));        
     //console.log(ddbData);  
 
-    data = [ 
-       { record: new Date(ddbData['Items'][4]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][4]['payload']['M']['distance'].S)}, 
-       { record: new Date(ddbData['Items'][3]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][3]['payload']['M']['distance'].S)},
-       { record: new Date(ddbData['Items'][2]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][2]['payload']['M']['distance'].S)},
-       { record: new Date(ddbData['Items'][1]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][1]['payload']['M']['distance'].S)},
-       { record: new Date(ddbData['Items'][0]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][0]['payload']['M']['distance'].S)}
-    ];
-  
-    console.log(data);    
+    data = [
+      //  { record: new Date(ddbData['Items'][4]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][4]['payload']['M']['distance'].S)}, 
+      //  { record: new Date(ddbData['Items'][3]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][3]['payload']['M']['distance'].S)},
+      //  { record: new Date(ddbData['Items'][2]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][2]['payload']['M']['distance'].S)},
+      //  { record: new Date(ddbData['Items'][1]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][1]['payload']['M']['distance'].S)},
+      //  { record: new Date(ddbData['Items'][0]['record'].N*1000).toLocaleString(), distance: parseFloat(ddbData['Items'][0]['payload']['M']['distance'].S)}
+      { record: new Date(ddbData['Items'][4]['record'].N * 1000).toLocaleString(), distance: parseFloat(ddbData['Items'][4]['distance'].S) },
+      { record: new Date(ddbData['Items'][3]['record'].N * 1000).toLocaleString(), distance: parseFloat(ddbData['Items'][3]['distance'].S) },
+      { record: new Date(ddbData['Items'][2]['record'].N * 1000).toLocaleString(), distance: parseFloat(ddbData['Items'][2]['distance'].S) },
+      { record: new Date(ddbData['Items'][1]['record'].N * 1000).toLocaleString(), distance: parseFloat(ddbData['Items'][1]['distance'].S) },
+      { record: new Date(ddbData['Items'][0]['record'].N * 1000).toLocaleString(), distance: parseFloat(ddbData['Items'][0]['distance'].S) }
+    ]; 
+    console.log(data);
+
+    ctx = document.getElementById("myChart").getContext('2d'); 
+
+    configuration = {
+      type: 'line',
+      data: {
+        labels: data.map((data) => data.record),
+        datasets: [
+          {
+            label: 'Distance',
+            data: data.map((data) => data.distance),
+            backgroundColor: [
+              "rgba(75,192,192,1)",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0"
+            ],
+            borderColor: "red",
+            borderWidth: 2,
+          }
+        ]
+      },
+      options: {
+        animation: {
+          duration: 0
+        }
+      }
+    }
+     if (myChart != null) myChart.destroy();
+     myChart = new Chart(ctx, configuration); 
+  } 
+
+  return (
+    <> 
+      <div className="App">
+      <canvas id="myChart" width="1024" height="512"></canvas> 
+      </div>
+    </>
+  );
+
+}
+
+    /*console.log(data);    
     
     ctx = document.getElementById("myChart").getContext('2d');
 
@@ -94,21 +140,25 @@ export default function App()
           }
         ]
       },
-            options: {
+  options: {
     animation: {
         duration: 0
     }
     }  
-  }
-    if(myChart != null) myChart.destroy();
-    myChart = new Chart(ctx, configuration);
+  } 
+
+  var table = new Tabulator("#example-table", {
+    data:data, //assign data to table
+    autoColumns:true, //create columns from data field names
+});
   }
 
   return (
-    <> 
+    <>  
       <div className="App">
       <canvas id="myChart" width="1024" height="512"></canvas> 
-      </div>
+      </div>.
+      <div id="example-table"></div> 
     </>
   );
-}
+}*/
