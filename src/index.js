6270069b57516a6c6ebf4b00bd4a4d78cded3b87
IOTAI ROBERTO <124071310+IOTAIROBERTO@users.noreply.github.com>
2025-10-09 09:26:19 -0700

    New Dashboard complete

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-// import React from "react";
-// import ReactDOM from "react-dom"; 
-// import App from "./App"; 
 import React from "react";
 import ReactDOM from "react-dom/client"; 
+import "./index.css"; 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   rootElement
-// );
-
-// setTimeout(function(){
-//   window.location.reload(5);
-// }, 5000);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
